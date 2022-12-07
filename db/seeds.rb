1e16692967f88c0bd9a748cc5ab04c9db8c73617
Your Name <you@example.com>
2022-12-07 13:48:29 +0300

    Add User to Devise

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+User.create(email: "vad@mail.ru", password: "123456", password_confirmation: "123456")
+
 10.times do |x| 
-  Post.create(title: "Title #{x}", body: "Body #{x}")
-end
+  Post.create(title: "Title #{x}", body: "Body #{x}", user_id: User.first.id)
+end 
